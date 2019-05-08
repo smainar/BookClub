dc948974e61ea871521667939c47dd7331d54618
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-08 16:37:24 -0600

    Add average_rating to book index view
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe "books index page", type: :feature do
     @book_1 = Book.create!(title: "Book 1", publication_year: 1999, pages: 100, cover_image: "book1.png")
     @book_2 = Book.create!(title: "Book 2", publication_year: 2000, pages: 105, cover_image: "book2.png")
     @book_3 = Book.create!(title: "Book 3", publication_year: 2001, pages: 110, cover_image: "book3.png")
+
+    @review_1 = @book_1.reviews.create!(username: "User 1", title: "Review Book 1.1", rating: 1, text: "Cool")
+    @review_2 = @book_1.reviews.create!(username: "User 2", title: "Review Book 1.2", rating: 2, text: "Cooler")
+    @review_3 = @book_2.reviews.create!(username: "User 3", title: "Review Book 3", rating: 3, text: "Coolest")
   end
 
   it "visitor sees all book titles, author, pages, and year in database" do
@@ -28,9 +32,9 @@ RSpec.describe "books index page", type: :feature do
     visit books_path
 
     expect(page).to have_content(@book_1.average_rating)
-    expect(page).to have_content(@book_1.reviews_count)
-
     expect(page).to have_content(@book_2.average_rating)
-    expect(page).to have_content(@book_2.reviews_count)
+
+    # expect(page).to have_content(@book_1.reviews_count)
+    # expect(page).to have_content(@book_2.reviews_count)
   end
 end
