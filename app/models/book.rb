a7d9548b7b49ef7ebda403ab931474da3f13ed3a
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-09 11:36:59 -0600

    Add sorting methods to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,4 +15,28 @@ class Book < ApplicationRecord
   def review_count
     reviews.count
   end
+
+  def self.order_average_rating(order_by_rating)
+    if order_by_rating == "asc"
+      select('books.*, avg(reviews.rating)').joins(:reviews).group('id').order('avg(reviews.rating)')
+    elsif order_by_rating == "desc"
+      select('books.*, avg(reviews.rating)').joins(:reviews).group('id').order('avg(reviews.rating) DESC')
+    end
+  end
+
+  def self.order_review_count(order_by_reviews)
+    if order_by_reviews == "asc"
+      select('books.*, count(reviews.id)').joins(:reviews).group('id').order('count(reviews.id)')
+    elsif order_by_reviews == "desc"
+      select('books.*, count(reviews.id)').joins(:reviews).group('id').order('count(reviews.id) DESC')
+    end
+  end
+
+  def self.order_page_count(order_by_pages)
+    if order_by_pages == "asc"
+      select('books.*').order(:pages)
+    elsif order_by_pages == "desc"
+      select('books.*').order(pages: :desc)
+    end
+  end
 end
