daa11e733ad0b38ca21d68025499f36cac912375
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-09 18:50:48 -0600

    - Deployed application to Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_213053) do
+ActiveRecord::Schema.define(version: 20190508213053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
