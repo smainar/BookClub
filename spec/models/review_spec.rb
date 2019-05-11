f8c391125033d360eacb98a257e969db7e9fa19b
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-11 16:55:28 -0600

    Add relationship to Review model (belongs_to :user)

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Review, type: :model do
 
   describe "relationships" do
     it { should belong_to :book }
+    it { should belong_to :user }
   end
 
   describe "class methods" do
