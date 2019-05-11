8ce595dc42a7f829d652b7ab22e6dcc5216e3407
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-10 19:49:24 -0600

    Add create, book_params, and author_params methods to book controller
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,30 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
+    book_title = book_params[:title].strip.downcase.titleize
+    books = Book.all
+    if books.titles.include?(book_title) || book_params[:pages].to_i <= 0
+      flash.notice = "Invalid entry. Please try again."
+      redirect_to new_book_path
+    else
+      book = Book.new(book_params)
+      book.update(title: book_title)
+      authors = author_params[:authors].titleize.split(", ")
+      authors.each do |author|
+        book.authors << Author.find_or_create_by(name: author)
+      end
+      book.save
+      redirect_to book_path(book)
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :publication_year, :pages, :cover_image)
+  end
+
+  def author_params
+    params.require(:book).permit(:authors)
   end
 end
--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "new book form", type: :feature do
 
     visit new_book_path
 
-    fill_in "book[title]", with: "The Talisman"
+    fill_in "book[title]", with: "the talisman"
     fill_in "book[publication_year]", with: 1984
     fill_in "book[pages]", with: 921
     fill_in "book[cover_image]", with: "https://images-na.ssl-images-amazon.com/images/I/411-vun7rML._SX320_BO1,204,203,200_.jpg"
-    fill_in "book[authors]", with: "Stephen King, Peter Straub"
+    fill_in "book[authors]", with: "stephen king, peter straub"
 
     click_on "Create Book"
 
@@ -22,5 +22,36 @@ RSpec.describe "new book form", type: :feature do
     expect(page).to have_content(new_book.pages)
     expect(page).to have_css("img[src='#{new_book.cover_image}']")
     expect(page).to have_content(new_book.authors.first.name)
+  end
+
+  it "displays flash notice when there is an invalid page entry" do
+    visit new_book_path
+
+    fill_in "book[title]", with: "the talisman"
+    fill_in "book[publication_year]", with: 1984
+    fill_in "book[pages]", with: -921
+    fill_in "book[cover_image]", with: "https://images-na.ssl-images-amazon.com/images/I/411-vun7rML._SX320_BO1,204,203,200_.jpg"
+    fill_in "book[authors]", with: "stephen king, peter straub"
+
+    click_on "Create Book"
+
+    expect(page).to have_content("Invalid entry. Please try again.")
+  end
+
+  it "displays flash notice when there is a duplicate title entry" do
+    b1 = Book.create!(title: "The Talisman", publication_year: 1984, pages: 200)
+
+    visit new_book_path
+
+    fill_in "book[title]", with: "the talisman"
+    fill_in "book[publication_year]", with: 1984
+    fill_in "book[pages]", with: 200
+    fill_in "book[cover_image]", with: "https://images-na.ssl-images-amazon.com/images/I/411-vun7rML._SX320_BO1,204,203,200_.jpg"
+    fill_in "book[authors]", with: "stephen king, peter straub"
+
+    click_on "Create Book"
+
+    expect(page).to have_content("Invalid entry. Please try again.")
+    expect(current_path).to eq(new_book_path)
   end
 end
