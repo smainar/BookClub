8570d37ffe22a539c183295b2813fbff658dfc03
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-12 02:02:27 -0600

    Add routes for authors show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/books', to: 'books#index'
   resources :books, only: [:index, :show, :new, :create]
+  resources :authors, only: [:show]
 end
