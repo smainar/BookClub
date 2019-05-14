ea4a0ebd8f29c841c234e39f9e4f33f0b75949a2
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-14 12:44:44 -0600

    Add params[:order_by_date] in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    if params[:order_by_date]
+      @reviews = @user.order_reviews(params[:order_by_date])
+    else
+      @reviews = @user.reviews
+    end
   end
 end
