25e0aa22f437f9376d725ba31257126dec34665b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-14 17:37:00 -0600

    Add routes for background controller and author destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/books', to: 'books#index'
+  get '/random_background.jpg', to: 'background#image'
   get '/', to: 'welcome#index'
+  
   resources :books, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
-  resources :authors, only: [:show]
+  resources :authors, only: [:show, :destroy]
   resources :reviews, only: [:destroy]
   resources :users, only: [:show] do
     resources :reviews, only: [:destroy]
