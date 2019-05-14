e4e112130ff294a9fa2100f9c04185c32448a028
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-14 12:42:36 -0600

    Add test to sort user's reviews by newest to oldest

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
-# As a Visitor,
-# When I click on a user's name for any book review
-# I am taken to a show page for that user.
-# I should see all reviews that this
-# user has written.
-# Each review shows:
-# - the title of the review
-# - the description of the review
-# - the rating of the review
-# - the title of the book
-# - the thumbnail image for the book
-# - the date the review was written
 
 RSpec.describe "user show page", type: :feature do
   before(:each) do
@@ -25,17 +13,14 @@ RSpec.describe "user show page", type: :feature do
 
     @user_1 = User.create!(username: "User 1")
     @user_2 = User.create!(username: "User 2")
-    @user_3 = User.create!(username: "User 3")
-    @user_4 = User.create!(username: "User 4")
 
     @users = User.all
 
     @review_1 = @book_1.reviews.create!(user: @user_1, title: "Review Book 1.1", rating: 1, text: "Cool")
     @review_2 = @book_1.reviews.create!(user: @user_2, title: "Review Book 1.2", rating: 2, text: "Cooler")
-    @review_3 = @book_2.reviews.create!(user: @user_3, title: "Review Book 3", rating: 3, text: "Coolest")
-    @review_4 = @book_2.reviews.create!(user: @user_1, title: "Review Book 1.3", rating: 5, text: "Best book ever")
-    @review_5 = @book_3.reviews.create!(user: @user_2, title: "Review Book 1.4", rating: 2, text: "Pretty bad")
-    @review_6 = @book_3.reviews.create!(user: @user_4, title: "Review Book 1.5", rating: 4, text: "Fun book")
+    @review_3 = @book_2.reviews.create!(user: @user_1, title: "Review Book 3", rating: 3, text: "Coolest")
+    @review_4 = @book_2.reviews.create!(user: @user_2, title: "Review Book 1.3", rating: 5, text: "Best book ever")
+    @review_5 = @book_3.reviews.create!(user: @user_1, title: "Review Book 1.4", rating: 2, text: "Pretty bad")
 
     @reviews = Review.all
   end
@@ -51,25 +36,109 @@ RSpec.describe "user show page", type: :feature do
         expect(page).to have_content(@review_1.text)
         expect(page).to have_content(@book_1.title)
         expect(page).to have_content(@review_1.rating)
-        expect(page).to have_content(@book_1.cover_image)
-        # expect(page).to have_content(DATE/TIME)
+        expect(page).to have_css("img[src='#{@book_1.cover_image}']")
+        expect(page).to have_content(@review_1.created_at.to_formatted_s(:long).slice(0...-6))
 
         expect(page).to_not have_content(@review_2.title)
         expect(page).to_not have_content(@review_2.text)
-        expect(page).to_not have_content(@book_2.title)
       end
+
+      visit user_path(@user_2)
+
+      expect(page).to have_content(@user_2.username)
 
       within "#review-#{@review_4.id}" do
         expect(page).to have_content(@review_4.title)
         expect(page).to have_content(@review_4.text)
         expect(page).to have_content(@book_2.title)
         expect(page).to have_content(@review_4.rating)
-        expect(page).to have_content(@book_2.cover_image)
-        # expect(page).to have_content(DATE/TIME)
+        expect(page).to have_css("img[src='#{@book_2.cover_image}']")
+        expect(page).to have_content(@review_4.created_at.to_formatted_s(:long).slice(0...-6))
 
         expect(page).to_not have_content(@review_5.title)
         expect(page).to_not have_content(@review_5.text)
-        expect(page).to_not have_content(@book_3.title)
+      end
+    end
+
+    it "displays a link to sort the user reviews by newest and by oldest " do
+      visit user_path(@user_1)
+
+      expect(page).to have_link("Most Recent - Reviews")
+      expect(page).to have_link("Oldest - Reviews")
+
+      click_on "Most Recent - Reviews"
+
+      expect(current_path).to eq(user_path(@user_1))
+    end
+
+    it "displays the user's reviews by newest to oldest" do
+      visit user_path(@user_1)
+
+      click_on "Most Recent - Reviews"
+
+      within "#review-#{@review_1.id}" do
+        expect(page).to have_content(@review_1.title)
+        expect(page).to have_content(@review_1.text)
+        expect(page).to have_content(@review_1.rating)
+        expect(page).to have_content(@book_1.title)
+        expect(page).to have_css("img[src='#{@book_1.cover_image}']")
+        expect(page).to have_content(@review_1.created_at.to_formatted_s(:long).slice(0...-6))
+
+      end
+
+      within "#review-#{@review_3.id}" do
+        expect(page).to have_content(@review_3.title)
+        expect(page).to have_content(@review_3.text)
+        expect(page).to have_content(@review_3.rating)
+        expect(page).to have_content(@book_2.title)
+        expect(page).to have_css("img[src='#{@book_2.cover_image}']")
+        expect(page).to have_content(@review_3.created_at.to_formatted_s(:long).slice(0...-6))
+
+      end
+
+      within "#review-#{@review_5.id}" do
+        expect(page).to have_content(@review_5.title)
+        expect(page).to have_content(@review_5.text)
+        expect(page).to have_content(@review_5.rating)
+        expect(page).to have_content(@book_3.title)
+        expect(page).to have_css("img[src='#{@book_3.cover_image}']")
+        expect(page).to have_content(@review_5.created_at.to_formatted_s(:long).slice(0...-6))
+
+      end
+    end
+
+    it "displays the user's review by oldest to newest" do
+      visit user_path(@user_1)
+
+      click_on "Oldest - Reviews"
+
+      within "#review-#{@review_5.id}" do
+        expect(page).to have_content(@review_5.title)
+        expect(page).to have_content(@review_5.text)
+        expect(page).to have_content(@review_5.rating)
+        expect(page).to have_content(@book_3.title)
+        expect(page).to have_css("img[src='#{@book_3.cover_image}']")
+        expect(page).to have_content(@review_5.created_at.to_formatted_s(:long).slice(0...-6))
+
+      end
+
+      within "#review-#{@review_3.id}" do
+        expect(page).to have_content(@review_3.title)
+        expect(page).to have_content(@review_3.text)
+        expect(page).to have_content(@review_3.rating)
+        expect(page).to have_content(@book_2.title)
+        expect(page).to have_css("img[src='#{@book_2.cover_image}']")
+        expect(page).to have_content(@review_3.created_at.to_formatted_s(:long).slice(0...-6))
+
+      end
+
+      within "#review-#{@review_1.id}" do
+        expect(page).to have_content(@review_1.title)
+        expect(page).to have_content(@review_1.text)
+        expect(page).to have_content(@review_1.rating)
+        expect(page).to have_content(@book_1.title)
+        expect(page).to have_css("img[src='#{@book_1.cover_image}']")
+        expect(page).to have_content(@review_1.created_at.to_formatted_s(:long).slice(0...-6))
       end
     end
   end
